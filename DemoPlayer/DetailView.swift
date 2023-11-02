3c894ca2e03800dc6dfb6e09c1b2598d8ee88802
Tomas <devtopol@icloud.com>
2023-11-02 12:32:09 +0100

    Detail view (title and description only) added to project.

---
+++ DemoPlayer/DetailView.swift
@@ -0,0 +1,24 @@
+//
+//  DetailView.swift
+//  DemoPlayer
+//
+//  Created by Tomas Pollak on 02/11/2023.
+//
+
+import SwiftUI
+
+struct DetailView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        
+        return UIViewController()
+        
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            
+        }
+}
+
+#Preview {
+    DetailView()
+}
